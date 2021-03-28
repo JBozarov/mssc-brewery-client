1557ad4d31af101d77c9d71c1fe74812fb880749
John Bozarov <jbozarov@teksystems.com>
2021-03-28 13:44:13 -0500

    Add Beer Post tested with other Beer Brewery project running

--- src/test/java/guru/springframework/msscbreweryclient/web/client/BreweryClientTest.java
+++ src/test/java/guru/springframework/msscbreweryclient/web/client/BreweryClientTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.net.URI;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -19,5 +20,13 @@ class BreweryClientTest {
     void getBeerById() {
         BeerDto dto = client.getBeerById(UUID.randomUUID());
         assertNotNull(dto);
+    }
+
+    @Test
+    void addNewBeer() {
+        BeerDto newBeer = BeerDto.builder().id(UUID.randomUUID()).beerName("Corona Beer").beerStyle("Mexican style").build();
+        URI uri = client.saveNewBeer(newBeer);
+        System.out.println(uri.toString()); ///api/v1/beer/e88cc6d5-a136-4c81-9629-43b86a911bdd UUID is random every time
+        assertNotNull(uri);
     }
 }
