51b6aeed863c8e946518fe0c634cbd7b8d6577f8
John Bozarov <jbozarov@teksystems.com>
2021-03-28 14:05:16 -0500

    updateBeer created tested using restTamplete put method

--- src/test/java/guru/springframework/msscbreweryclient/web/client/BreweryClientTest.java
+++ src/test/java/guru/springframework/msscbreweryclient/web/client/BreweryClientTest.java
@@ -17,16 +17,22 @@ class BreweryClientTest {
     BreweryClient client;
 
     @Test
-    void getBeerById() {
+    void testGetBeerById() {
         BeerDto dto = client.getBeerById(UUID.randomUUID());
         assertNotNull(dto);
     }
 
     @Test
-    void addNewBeer() {
+    void testSaveNewBeer() {
         BeerDto newBeer = BeerDto.builder().id(UUID.randomUUID()).beerName("Corona Beer").beerStyle("Mexican style").build();
         URI uri = client.saveNewBeer(newBeer);
         System.out.println(uri.toString()); ///api/v1/beer/e88cc6d5-a136-4c81-9629-43b86a911bdd UUID is random every time
         assertNotNull(uri);
+    }
+
+    @Test
+    void testUpdateBeer() {
+        BeerDto beerDto = BeerDto.builder().beerName("New Beer").build();
+        client.updateBeer(UUID.randomUUID(), beerDto);
     }
 }
